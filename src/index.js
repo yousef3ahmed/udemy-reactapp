ba4ede0eca8ba04b6bb85a1235dcd98f0bb37303
yousef ahmed <yousef.amer5453@gmail.com>
2022-09-19 02:11:54 +0200

    end point

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   
-  <BrowserRouter>
+  <BrowserRouter  basename={process.env.PUBLIC_URL} >
     <Routes>
        <Route path="/" element={<FinalHome />} />
        <Route path="/test" element={<Test />} />
